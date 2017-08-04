83c59e40863f13cbb8ea3aa8e374055695088d87
Shweta Bansal <ershwetabansal@energyaspects.com>
2017-08-04 20:08:39 +0100

    Create base project

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    uri: 'http://www.canarywharfcollege.co.uk/open-days/929.html',
+    ids: [],
+    tags: ['table']
+  },
+  {
+    uri: 'http://www.canarywharfcollege.co.uk/open-days/930.html',
+    ids: [],
+    tags: ['table']
+  }
+]
---
+++ index.js
@@ -0,0 +1,22 @@
+const Crawler = require('crawler')
+const fs = require('fs')
+const config = require('./config.js')
+
+const c = new Crawler({
+  maxConnections : 10,
+  callback : (error, res, done) => {
+    if(error){
+      console.log(error)
+    }else{
+      const $ = res.$
+      res.options.tags.forEach(tag => {
+        console.log($(tag).html())
+      })
+      res.options.ids.forEach(id => {
+        console.log($('#'+id).html())
+      })
+    }
+    done()
+  }
+})
+c.queue(config)
